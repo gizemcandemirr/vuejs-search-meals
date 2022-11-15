4586d08a6b0d8e7c92c66b30feacca4d1cf23060
Gizem Candemir <gizemfener2727@gmail.com>
2022-11-15 22:45:04 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="p-8 flex flex-col">
+    <div>
+      <input
+        class="rounded border-2 border-gray-200 w-full"
+        type="text"
+        placeholder="Search for Meals"
+      />
+    </div>
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+      >
+        {{ letter }}</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+
+const meals = computed(() => store.state.meals);
+const letters = "ABJFGJEFLJGFFGŞFLG".split("");
+</script>
---
+++ src/views/MealList.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Vuee</div>
+</template>
+
+<script setup></script>
