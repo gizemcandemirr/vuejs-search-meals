8e56a7f1834536c586d4e014ca34ea4cd838ad43
Gizem <gizemfener2727@gmail.com>
2022-11-29 12:28:14 +0300

    meals list

--- src/views/SearchByName.vue
+++ src/views/SearchByName.vue
@@ -1,20 +1,23 @@
 <template>
   <div class="p-6">
-    <input
-      type="text"
-      v-model="keyword"
-      class="rounded border-2 border-gray-200 w-full"
-      placeholder="Search for Meals"
-      @change="searchMeals"
-    />
+    <input type="text" v-model="keyword" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals"
+      @change="searchMeals" />
   </div>
-  <div>
-    {{ meals }}
+
+  <div v-for="meal of meals" :key="meal.idMeal">
+    <div class="text-center">{{ meal.strMeal }}</div>
+    <router-link :to="{ name: 'meal', params: { id: meal.idMeal } }">
+      <div class="flex flex-col items-center">
+        <img :src="meal.strMealThumb" class="w-32 h-32" />
+        <div class="text-center">{{ meal.strMeal }}</div>
+      </div>
+    </router-link>
   </div>
+
 </template>
 
 <script setup>
-import { computed,ref } from "vue";
+import { computed, ref } from "vue";
 import store from "../store";
 
 const keyword = ref("");
